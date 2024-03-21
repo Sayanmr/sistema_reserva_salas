6ef1833304eb95686ccf48bc59e4bb1268cb45dd
Sayanmr <160552703+Sayanmr@users.noreply.github.com>
2024-03-20 21:08:12 -0500

    Avance en la cartelera

---
+++ peliculas/side.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div class="box">
+<div class="a">
+    <img width="500px"src="peliculasimg\MV5BMTYyOTAxMDA0OF5BMl5BanBnXkFtZTcwNzgwNTc1NA@@._V1_.jpg" alt="Insidious">
+</div>
+<video autoplay muted width="320" height="240" class=trailer>
+    <source src="trailers/insidious.mp4" type="video/mp4">
+</video>
+    <div class="capa"></div>
+</div>
+</body>
+</html>
